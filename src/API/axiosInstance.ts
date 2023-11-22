4fc991782720b3aa97d8ffdf5598fe6a03e62688
leeool <lgonsalez02@gmail.com>
2023-11-21 23:37:03 -0300

    feat: update url

--- src/API/axiosInstance.ts
+++ src/API/axiosInstance.ts
@@ -1,12 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://dark-hose-lion.cyclic.app",
   headers: {
     "Content-Type": "application/json",
     "data-type": "jsonp",
-    "Access-Control-Allow-Origin": "*"
-  }
-})
+    "Access-Control-Allow-Origin": "*",
+  },
+});
 
-export default axiosInstance
+export default axiosInstance;
