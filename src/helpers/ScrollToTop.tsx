b3cbb797768cdbd496fe88079ccfc5fd0b4de4fb
leeool <lgonsalez02@gmail.com>
2023-11-21 23:45:28 -0300

    feat: add scroll to top

--- src/helpers/ScrollToTop.tsx
+++ src/helpers/ScrollToTop.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = ({ children }: { children: React.ReactNode }) => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  const loc = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [loc.pathname]);
 
   return <>{children}</>;
 };
